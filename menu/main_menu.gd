400090e8d2726a56a1c1bfd407a063fc087b128a
Manuel Otero <manuelOtero.af@gmail.com>
2025-11-15 17:56:25 +0100

    Menus v01

---
+++ menu/main_menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var level = preload("res://testing.tscn")
+
+var is_changing_scene = false
+
+func change_scene(next_scene:PackedScene):
+	is_changing_scene = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(self,"modulate:a", 0, 1)
+	await tween.finished
+	get_tree().change_scene_to_packed(next_scene)
+	
+	
+
+
+func _on_btn_start_pressed() -> void:
+	if not is_changing_scene:
+		change_scene(level)
+
+func _on_btn_credits_pressed() -> void:
+	pass # Replace with function body.
