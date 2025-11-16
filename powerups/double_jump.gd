55c9e7a037505c3f37d23798f39f9e08d58fa026
x0tero <gxoelotero@gmail.com>
2025-11-16 18:44:50 +0100

    itemConsegudo
    itemConsegudo

--- powerups/double_jump.gd
+++ powerups/double_jump.gd
@@ -1,7 +1,9 @@
 extends Node2D
+@onready var sound = $"../itemConseguido"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.is_active_double_jump = true
+		sound.play()
 		
 		queue_free()
